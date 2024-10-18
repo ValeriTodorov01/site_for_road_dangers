5bfd434f05b13bc6e55613829f502ec2aa3d1f79
ValeriTodorov123 <valerytodorov5@gmail.com>
2024-10-18 14:47:34 +0300

    adding footer

--- roadDangers/src/App.tsx
+++ roadDangers/src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Map from "./components/Map";
 import Footer from "./components/Footer";
 
+//https://www.figma.com/design/mqc0FKzCs3hepP2FW2Ln58/Untitled?node-id=0-1&node-type=canvas&t=K4xzjga9G6JlQnW2-0
+
 function App() {
 	return (
 		<>
--- roadDangers/src/components/Footer.tsx
+++ roadDangers/src/components/Footer.tsx
@@ -3,15 +3,33 @@ import { CiMap } from "react-icons/ci";
 
 const Footer = () => {
     return (
-        <footer className="font-semibold text-[#344050] mt-12 px-8">
-            <div className="flex items-center">
+        <footer className="font-semibold text-[#344050] mt-12 mb-20 px-9">
+            <div className="flex items-center mb-4">
 				<CiMap size={48} />
 				<h1 className="text-3xl">Name</h1>
 			</div>
 
             <div className="flex justify-between">
                 <p className="text-[20px]">Opisanie na website - blalalfdpifijoighojo</p>
+                <div className="flex gap-20">
+                    <div className="flex-col">
+                        <div className="flex items-center gap-2 mb-2">
+                            <div className="bg-[#344050] w-1 h-12"></div>
+                            <h2 className="text-xl">Контакти</h2>
+                        </div>
+                        <p>Телефон: 0888 888 888</p>
+                        <p>Email: val@gmail.com</p> 
+                    </div>
 
+                    <div className="flex-col">
+                        <div className="flex items-center gap-2 mb-2">
+                            <div className="bg-[#344050] w-1 h-12"></div>
+                            <h2 className="text-xl">Контакти</h2>
+                        </div>
+                        <p>Телефон: 0888 888 888</p>
+                        <p>Email: val@gmail.com</p> 
+                    </div>
+                </div>
             </div>
         </footer>
     );
